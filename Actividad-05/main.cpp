bd317f98307222a23b0422ee336a5b6a32c4d530
abrahamfourteen <36650502+abrahamfourteen@users.noreply.github.com>
2020-09-21 14:01:47 -0500

    Archivo en C++ para poder agregar archivos .h

---
+++ Actividad-05/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  cout << "Hola github!\n";
+
+  string nombre;
+  cout << "Nombre : "; 
+  getline(cin, nombre);
+  cout << "Hola como estas " << nombre << '\n';
+
+  int edad;
+  cout << "Edad : ";
+  cin >> edad;
+  cout << "La edad de " << nombre << " es " << edad << '\n';
+  
+  return 0;
+}
